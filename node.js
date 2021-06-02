9ed3f6e72898f725196809e206a65d41e762145f
Lateeftheshyboy <81362674+Lateeftheshyboy@users.noreply.github.com>
2021-06-02 12:28:22 +0100

    Add files via upload

---
+++ node.js
@@ -0,0 +1,17 @@
+//const name = 'Lateef';
+//const age = 27;
+
+//Concatenation
+
+//console.log('My name is ' + name  + ' and I am ' + age);
+
+// Template String
+//console.log(`My name is ${name} and I am ${age} `);
+
+var isDating = false;
+
+if (!isDating) {
+    console.log('Happily in a relationship');
+} else {
+    console.log('Single to stupor');
+}
